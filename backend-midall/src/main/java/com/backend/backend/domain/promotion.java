ce3d694ff7b4acd11eb7cba604c887299c675cc0
Victor Cavichioli <vivictoraraujo@hotmail.com>
2022-03-20 12:39:36 -0300

    endpoints/DTOs checked

---
+++ backend-midall/src/main/java/com/backend/backend/domain/promotion.java
@@ -0,0 +1,14 @@
+package com.backend.backend.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class promotion {
+
+    private Integer id;
+
+    private Double discount;
+
+}
