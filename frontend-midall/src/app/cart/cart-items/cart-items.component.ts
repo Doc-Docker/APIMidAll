b3d20f286e3794990c13147c13d87987806d02e7
Victor Cavichioli <vivictoraraujo@hotmail.com>
2022-06-09 20:41:53 -0300

    Fix discount return DR

--- frontend-midall/src/app/cart/cart-items/cart-items.component.ts
+++ frontend-midall/src/app/cart/cart-items/cart-items.component.ts
@@ -4,6 +4,7 @@ import { ProductsService } from 'src/app/products.service';
 import { Product } from 'src/app/products/Product';
 import { Cart } from '../Cart';
 import { Observable } from 'rxjs';
+import { ProductDTO } from 'src/app/products/ProductDTO';
 
 @Component({
   selector: 'app-cart-items',
@@ -22,7 +23,9 @@ export class CartItemsComponent implements OnInit {
   id : number;
   quantidade : number;
   total: number;
+  lista: Product[] = [];
 
+  teste : ProductDTO[] = []
   constructor(private service : CartService) { }
 
   ngOnInit(): void {
@@ -31,43 +34,49 @@ export class CartItemsComponent implements OnInit {
     this.product;
     this.discount;
     this.noDiscount = 0;
-
+    this.lista = [];
+    this.teste = [];
 
     Cart.products.forEach(element => {
-      this.product= element;
+      this.product = element;
       this.id  = element.id;
       this.quantidade = element.quantidade;
       this.products.push(element);
-
-      if(this.product.promotion != []){
 
         this.total = this.noDiscount += (element.price  * element.quantidade);
 
         this.service.getDiscount(this.id, this.quantidade, this.total).subscribe(
             response => 
-            { this.discount = response,
-            this.finalPrice = this.finalPrice += (element.price * element.quantidade)-(this.discount);
+            { const productDto : ProductDTO = new ProductDTO();
+              productDto.discount = response
+              productDto.name = element.name;
+              productDto.price = element.price;
+              productDto.quantity = element.quantidade;
+              this.discount = response;
+              this.teste.push(productDto)
+              this.finalPrice = this.finalPrice += (element.price * element.quantidade)-(this.discount)
             
             errorResponse => console.log(errorResponse)
         })
-        element.promotion = this.discount;
-        this.noDiscount = this.total;
-      }
-      else {
-        this.finalPrice += (element.price * element.quantidade);
-      }
     });
 
-  
   }
 
   deleteProduct(product : Product){
-
-
     let position = Cart.products.indexOf(product);
     if (position !== -1) {
       Cart.products.splice(position, 1);
   }
+    this.ngOnInit();
+  }
+
+  deleteProductDto(product : ProductDTO){
+    let position = this.teste.indexOf(product)
+    if (position !== -1) {
+      this.teste.splice	(position, 1)
+
+    }
+
     this.ngOnInit();
   }
 }
